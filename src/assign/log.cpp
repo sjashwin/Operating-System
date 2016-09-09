32d353c8890046b0a2bb2b12c1d2903394dfec75
Balasubramanian <balathinnappan@gmail.com>
2016-09-09 18:45:36 +0530

    comments added

--- src/assign/log.cpp
+++ src/assign/log.cpp
@@ -6,30 +6,30 @@
 int main(int argc, char *argv[]){
 	int c, x ;
 	std::string filename = "./logfile.txt", filename_temp ;
-	std::ofstream logfile ;
-	logfile.open(filename.c_str()) ;
+	std::ofstream logfile ;//to write to a file. class name ofstream
+	logfile.open(filename.c_str()) ;//openeing the file.
 	//refer to http://man7.org/linux/man-pages/man3/getopt.3.html
-	while ((c =getopt(argc, argv, "-hnl"))!= -1){
+	while ((c =getopt(argc, argv, "-hnl"))!= -1){//getopt parses the command line flags and args.
 		switch(c){
-			case 'h':
+			case 'h'://help
 				std::cout << "-h - help\n-n x to assign a value\n-l - to assign file name." << std::endl ;
 				break ;
-			case 'n':
+			case 'n'://setting the value of the file
 				x = atoi(argv[optind]) ;
 				std::cout << x << std::endl ;
 				logfile << "x = "<< x << std::endl ;
 				break ;
-			case 'l':
+			case 'l'://to rename the file.
 				filename_temp = argv[optind] ;
 				std::cout<< filename_temp << std::endl ;
 				std::rename( filename.c_str(), filename_temp.c_str() ) ;
 				filename = filename_temp ;
 				break ;
 			case '?':
-				std::cout << "not available"<<std::endl ;
+				std::cout << "not available"<<std::endl ;//error
 				break ;
 			case ':':
-				std::cout << "argument missing" << std::endl ;
+				std::cout << "argument missing" << std::endl ;//error
 				break ;
 		}
 	}
